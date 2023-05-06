1ee7b89c0500b406435d7820dff67a66a22463be
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-06 23:46:35 +0800

    pseudocode

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,6 +1,5 @@
 # Simple Calculator #
 # Create a Simple App Calculator
-# Create a Simple App Calculator
 # 1. The application will ask the user to choose one of the four math operations (Addition, Subtraction, Multiplication and Division)
 # 2. The application will ask the user for two numbers
 # 3. Display the result
@@ -8,4 +7,11 @@
 # 5. If yes, repeat Step 1.
 # 6. If no, Display “Thank you!” and the program will exit 
 # 7. Use Python Function and appropriate Exceptions to capture errors during runtime.
-#               
+
+# Ask user to use what arithmetic operation to use via input
+# Ask user to input two  numbers to conduct the arithmetic operation
+# Print the Result
+# Ask user whether to try again or not
+# Repeat if yes
+# End program with "Thank you!" with no
+# Use defined python functions and exceptions to fix errors
