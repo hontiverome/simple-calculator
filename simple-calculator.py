7ea1bc527abcc6b5480c4c9b6cbd5f26bc537617
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-07 00:15:22 +0800

    added module,try,zerodivision exception,andreturn.

--- simple-calculator.py
+++ simple-calculator.py
@@ -9,37 +9,47 @@
 # 7. Use Python Function and appropriate Exceptions to capture errors during runtime.
 
 # Ask user to use what arithmetic operation to use via input
-while True:
-    arithmetic_operation=str(input("Type the symbol of the arithmetic operation you would like to use\n( + , - , * , / )\n: "))
-    if arithmetic_operation=='+':
-        print("You chose addition")
-    elif arithmetic_operation=='-':
-        print("You chose subtraction")
-    elif arithmetic_operation=='*':
-        print("You chose multiplication")
-    elif arithmetic_operation=='/':
-        print("You chose division")
-    else:
-        print("Invalid.\n")
-        continue
-    
-    firstN=int(input("\nWhat is your first number?\n"))
-    secondN=int(input("\nWhat is your second number?\n"))
-    
-    if arithmetic_operation=='+':
-        result=firstN+secondN
-    elif arithmetic_operation=='-':
-        result=firstN-secondN
-    elif arithmetic_operation=='*':
-        result=firstN*secondN
-    elif arithmetic_operation=='/':
-        result=firstN/secondN
-    
-    print(f"Your inputs gathered the value\n:{result}")
-    
-    restart=str(input("Would you like to try again? (y or n only.)\n:"))
-    if restart.lower()=='n':
-        break
+def SimpleCalculator():
+    while True:
+        try:
+            arithmetic_operation=str(input("Type the symbol of the arithmetic operation you would like to use\n( + , - , * , / )\n: "))
+            if arithmetic_operation=='+':
+                print("You chose addition")
+            elif arithmetic_operation=='-':
+                print("You chose subtraction")
+            elif arithmetic_operation=='*':
+                print("You chose multiplication")
+            elif arithmetic_operation=='/':
+                print("You chose division")
+            else:
+                print("Invalid.\n")
+                continue
+            
+            firstN=int(input("\nWhat is your first number?\n"))
+            secondN=int(input("\nWhat is your second number?\n"))
+            
+            if arithmetic_operation=='+':
+                result=firstN+secondN
+            elif arithmetic_operation=='-':
+                result=firstN-secondN
+            elif arithmetic_operation=='*':
+                result=firstN*secondN
+            elif arithmetic_operation=='/':
+                result=firstN/secondN
+            
+            print(f"Your inputs gathered the value\n:{result}")
+            
+            restart=str(input("Would you like to try again? (y or n only.)\n:"))
+            while True:
+                if restart.lower()=='n':
+                    return
+                elif restart.lower()=='y':
+                    break
+                else:
+                    print("Invalid.\nTry again? (y/n)\n:")
+        except ZeroDivisionError:
+            print("Sorry! You can't divide by zero.")
+            
     
 # Ask user to input two numbers to conduct the arithmetic operation
 # Print the Result
