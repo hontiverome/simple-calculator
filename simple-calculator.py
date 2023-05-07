a6a0bde671f1434c26aa883900a4fa5950f3c0c5
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-05-07 15:44:35 +0800

    added option to enable animations

--- simple-calculator.py
+++ simple-calculator.py
@@ -9,7 +9,7 @@
 # 7. Use Python Function and appropriate Exceptions to capture errors during runtime.
 import time
 
-def SimpleCalculator():
+def AnimatedSimpleCalculator():
     while True:
         # Use defined python functions and exceptions to fix errors
         try:
@@ -81,8 +81,80 @@ def SimpleCalculator():
             # Displays 'Thank you!' whether if the user wants to exit or try again
             print("\nThank you!")
 
+def SimpleCalculator():
+    while True:
+        # Use defined python functions and exceptions to fix errors
+        try:
+            # Ask user to use what arithmetic operation to use via input
+            arithmetic_operation = str(input("\nType the symbol of the arithmetic operation you would like to use\n( + , - , * , / )\n: "))
+            if arithmetic_operation == '+':
+                print("\nYou chose addition")
+                operation='Addition'
+            elif arithmetic_operation == '-':
+                print("\nYou chose subtraction")
+                operation='Subtraction'
+            elif arithmetic_operation == '*':
+                print("\nYou chose multiplication")
+                operation='Multiplication'
+            elif arithmetic_operation == '/':
+                print("\nYou chose division")
+                operation='Division'
+            else:
+                print("Invalid.\n")
+                continue
 
-SimpleCalculator()
+            # Ask user to input two numbers to conduct the arithmetic operation
+            firstN = float(input("\nWhat is your first number?\n"))
+            secondN = float(input("\nWhat is your second number?\n"))
+
+            # Computes the two numbers using the operation specified
+            if arithmetic_operation == '+':
+                result = firstN+secondN
+            elif arithmetic_operation == '-':
+                result = firstN-secondN
+            elif arithmetic_operation == '*':
+                result = firstN*secondN
+            elif arithmetic_operation == '/':
+                result = firstN/secondN
+
+            print('\nOperation')
+            print(f': {operation}')
+            print(f"Numbers to be computed")
+            print(f': {firstN, secondN}')
+            # Print the Result
+            print(f"\n\nHere is your result\n:{result}")
+
+            # Ask user whether to try again or not
+            restart = str(
+                input("\nWould you like to try again? (y or n only.)\n:"))
+            while True:
+                # End if no
+                if restart.lower() == 'n':
+                    return
+                # Repeat if yes
+                elif restart.lower() == 'y':
+                    break
+                else:
+                    print("\nInvalid.\nTry again? (y/n)\n:")
+
+        except ZeroDivisionError:
+            print("Sorry! You can't divide by zero.")
+        except ValueError:
+            print("Sorry! Numbers only.")
+        finally:
+            # Displays 'Thank you!' whether if the user wants to exit or try again
+            print("\nThank you!")
+                  
+while True:
+    animated=str(input("Would you like to have animations? (y/n)\n: "))    
+    if animated.lower()=='y':
+        AnimatedSimpleCalculator()
+        exit()
+    elif animated.lower()=='n':
+        SimpleCalculator()
+        exit()
+    else:
+        print("Invalid.\n")
 
 #HONTIVEROS JEROME ANDREI O.
 #BSCPE 1-5
